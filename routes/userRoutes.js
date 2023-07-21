1eaf8432fc00bbda49f2d569aff4031090e67a93
Romain <romain.gojard@gmail.com>
2023-07-21 10:34:20 +0200

    Correction middleware on routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const auth = require('../middlewares/auth');
 
 router.post('/login',auth, authController.login);
 router.get('/users',auth, userController.getAllUsers);
